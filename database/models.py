a59a531b122a9fcc4def625322b52b6d89cca012
nadav7679 <nadav76799@gmail.com>
2021-09-13 13:09:54 +0300

    Working version of SQL tables

--- database/models.py
+++ database/models.py
@@ -8,12 +8,12 @@ class Soldier(Base):
     __tablename__ = "soldiers"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
     favorite_anime = Column(String)
-    department_id = Column(String, ForeignKey("Department"))
+    department = Column(String, ForeignKey("departments.name"))
 
     commander = relationship("Soldier", remote_side=[name])
-    department = relationship("Department", back_populates="slaves")
+    _department = relationship("Department", back_populates="slaves")
 
     def __repr__(self):
         return f'<Soldier: {self.name}, owner: {self.commander}>'
@@ -23,9 +23,10 @@ class Department(Base):
     __tablename__ = "departments"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    king = Column(String, ForeignKey("soldiers.name"), nullable=False)
 
-    slaves = relationship("Soldier", back_populates="department")
+    _king = relationship("Soldier", back_populates="department")
 
     def __repr__(self):
         return f'<Department: {self.name}>'
--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import dotenv
 from sqlalchemy import create_engine
 from config import app_config
 
-from database import models
+from database.models import Base
 
 
 dotenv.load_dotenv("./.env")
 env_vars = os.environ
 env = env_vars['ENV']
 
-os.system(f"docker run --rm -d -e POSTGRES_PASSWORD={env_vars['DB_PASSWORD']} -e POSTGRES_DB={env_vars['DATABASE']}"
+os.system(f"docker run --rm -d -e POSTGRES_USER={env_vars['DB_USERNAME']}"
+          f" -e POSTGRES_PASSWORD={env_vars['DB_PASSWORD']} -e POSTGRES_DB={env_vars['DATABASE']}"
           f" -p 5432:5432 --name db_manager postgres")
 time.sleep(2)
 
 engine = create_engine(app_config[env].SQLALCHEMY_URI)
-
-models.Base.metadata.create_all(bind=engine)
+print(app_config[env].SQLALCHEMY_URI)
+Base.metadata.create_all(bind=engine)
