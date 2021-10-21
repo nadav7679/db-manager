52725add255ecf8925685ac9a935e46ec9fc11de
nadav7679 <nadav76799@gmail.com>
2021-10-21 16:35:16 +0300

    Created basic GET backend

--- backend/classes.py
+++ backend/classes.py
@@ -1,6 +1,50 @@
+from database import session
+from database.models import classnames
+from sqlalchemy import select, text, desc, asc
+
+
 class Get:
 
     def __init__(self, filter):
-        self.tablename = filter.tablename
-        self.where = filter.where
-        self.order_by = filter.order_by
+        self.schema = filter["schema"]
+        self.table = filter["table"]
+        self.columns = filter["columns"]
+        self.where = filter["where"]
+        self.orderBy = filter["orderBy"]
+        self.limit = filter["limit"]
+
+    def get_data(self):
+
+        table = classnames[self.table]
+        if self.columns is None:
+            select_stmt = select(table)
+
+        else:
+            attr = [getattr(table, column) for column in self.columns]
+            select_stmt = select(*attr)
+
+        if self.where is not None:
+            stmt = select_stmt.where(text(self.where))
+
+        if self.orderBy is not None:
+            stmt = stmt.order_by()
+
+        if self.limit is not None:
+            stmt
+
+        print(stmt)
+        res = session.execute(stmt)
+        print(res.fetchall())
+
+
+soldier = Get({
+    "schema": "public",
+    "table": "departments",
+    "columns": ["id", "name"],
+    "where": "departments.id > 5",
+    "orderBy": None,
+    "limit": None
+}
+)
+
+soldier.get_data()
